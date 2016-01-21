5af852878c0732c557272bb69cca795aaeb27261
spawner999 <lore.ferrario@gmail.com>
2016-01-21 21:14:29 +0100

    refactor pingpong() function, add a test to check input and delete two tests that will be added to a different suite later

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,4 @@
 var pingPong = function(number){
-	if(typeof number !== 'number'){
-		return 'error';
-	}
-	if(number < 1){
-		return 'error';
-	}
-	if(number % 15 ===0){
-		return 'pingpong';
-	}
-	if(number % 3 === 0){
-		return 'ping';
-	}
-	if(number % 5 === 0){
-		return 'pong';
-	}
-	else return number;
+	return(number % 3 === 0? (number % 5 === 0? 'pingpong' : 'ping') : (number % 5 === 0? 'pong' : number));
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,19 @@
 describe('pingPong', function(){
- it("returns ping if number is divisible by 3", function(){
-  expect(pingPong(6)).to.equal("ping");
+
+ it('returns ping if number is divisible by 3', function(){
+  expect(pingPong(3)).to.equal("ping");
  });
- it("returns pong if number is divisible by 5", function(){
-  expect(pingPong(10)).to.equal("pong");
+
+ it('returns pong if number is divisible by 5', function(){
+  expect(pingPong(5)).to.equal("pong");
  });
- it("returns pingpong if number is divisible by both 3 and 5", function(){
-  expect(pingPong(30)).to.equal("pingpong");
+
+ it('returns pingpong if number is divisible by both 3 and 5', function(){
+  expect(pingPong(15)).to.equal("pingpong");
  });
- it("returns error if number is not a number", function(){
- 	expect(pingPong('ji')).to.equal("error");
+
+ it('returns number if none of the above is true', function(){
+  expect(pingPong(17)).to.equal(17);
  });
- it("returns error if number is less than 1", function(){
- 	expect(pingPong(-4)).to.equal("error");
- })
+
 });
