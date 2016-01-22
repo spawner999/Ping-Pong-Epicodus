96721c98f6af34470718690eb8ebee6be740a947
spawner999 <lore.ferrario@gmail.com>
2016-01-22 16:37:38 +0100

    add appendResult function to test pingpong, and even listener to input form

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,18 @@ var isValidNumber = function(input){
 var pingPong = function(number){
 	return(number % 3 === 0? (number % 5 === 0? 'pingpong' : 'ping') : (number % 5 === 0? 'pong' : number));
 };
+
+var appendResult = function(number){
+	for(var i=1; i<=number; i++){
+		console.log(pingPong(i));
+	}
+};
+
+$(document).ready(function(){
+	$("form#input-form").submit(function(event) {
+  	var userInput = parseInt($("input#user-input").val());
+  	appendResult(userInput);
+  	$('input#user-input').val('');
+    event.preventDefault();
+});
+});
