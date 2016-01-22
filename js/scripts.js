37368b0cd0e9697506cde96453af96804105b1c3
spawner999 <lore.ferrario@gmail.com>
2016-01-22 17:11:24 +0100

    modify appendResult function to add <li> to the dom, add function to print error msg

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,31 @@
 var isValidNumber = function(input){
-	return(typeof input === 'number' && input > 0);
+  return(typeof input === 'number' && input > 0);
 };
 
 var pingPong = function(number){
-	return(number % 3 === 0? (number % 5 === 0? 'pingpong' : 'ping') : (number % 5 === 0? 'pong' : number));
+  return(number % 3 === 0? (number % 5 === 0? 'pingpong' : 'ping') : (number % 5 === 0? 'pong' : number));
 };
 
 var appendResult = function(number){
-	for(var i=1; i<=number; i++){
-		console.log(pingPong(i));
-	}
+  for(var i=1; i<=number; i++){
+    $('#results').append('<li>' + pingPong(i) + '</li>');
+  }
+};
+
+var appendErrorMsg = function(){
+  $('#results').append('<li>The number you entered is invalid. Please try entering a different one.</li>');
 };
 
 $(document).ready(function(){
-	$("form#input-form").submit(function(event) {
-  	var userInput = parseInt($("input#user-input").val());
-  	appendResult(userInput);
-  	$('input#user-input').val('');
+  $('form#input-form').submit(function(event) {
+    $('#results').empty();
+    var parsedInput = parseInt($('input#user-input').val());
+    if(isValidNumber(parsedInput)) {
+      appendResult(parsedInput);
+    }
+    else {
+      appendErrorMsg();
+    }
     event.preventDefault();
-});
+  });
 });
